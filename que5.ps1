2081a260bf6a51882bfc1f0bfb334bc131856758
Iamneo <techadmin@iamneo.ai>
2023-09-13 05:21:13 +0000

    Backup Commit

--- que5.ps1
+++ que5.ps1
@@ -1,11 +1,15 @@
 mkdir Testing
 cd Testing
 for($i = 1 ; $i -le 2 ; $i++){
-    mkdir Subfolder+$i
+    mkdir SubFolder$i
 }
+cd SubFolder1
 for($j = 1 ; $j -le 3 ; $j++){
-    new-item Test+$j.txt
+    new-item Test$j.txt
 }
+cd ..
+cd SubFolder2
 for($k = 4 ; $k -le 6 ; $k++){
-    new-item Test+$k.txt
+    new-item Test$k.txt
 }
+cd ..
